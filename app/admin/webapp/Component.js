c559793d1010a03b2215b2a1b1df899ab33da856
kla-ko <>
2020-12-04 16:45:53 +0100

    Align with cds-dk preview

--- app/admin/webapp/Component.js
+++ app/admin/webapp/Component.js
@@ -1,6 +1,6 @@
 sap.ui.define(['sap/fe/core/AppComponent'], function(AppComponent) {
     'use strict';
     return AppComponent.extend('admin.Component', {
-        metadata: {  manifest: 'json'  }
+        metadata: {  manifest: 'manifest.json'  }
     });
 });
